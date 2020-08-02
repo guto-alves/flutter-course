bb15dfffce19f4b099e3cd03fa504a9a874d7496
Guto Alves <gustavo.almeida13@fatec.sp.gov.br>
2020-08-02 17:00:17 -0300

    Add Dicee App and the first challenge - Magic 8 Ball

---
+++ magic-8-ball/lib/main.dart
@@ -0,0 +1,47 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int _ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            _ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$_ballNumber.png'),
+      ),
+    );
+  }
+}
