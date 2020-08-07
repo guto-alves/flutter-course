048fe5b9ba42a5f81b484524480950e0108ff140
Guto Alves <gustavo.almeida13@fatec.sp.gov.br>
2020-08-07 05:48:48 -0300

    Solve the boss level challenge 2 - Destini

---
+++ destini-challenge/lib/story.dart
@@ -0,0 +1,11 @@
+class Story {
+  String title;
+  String choice1;
+  String choice2;
+
+  Story({String title, String choice1, String choice2}) {
+    this.title = title;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
+  }
+}
